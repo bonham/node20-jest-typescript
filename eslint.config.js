f49d27d206c957410040692c759456e3506dfb5e
Art Furrer <2823246+bonham@users.noreply.github.com>
2024-02-17 00:45:07 +0100

    everything works

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,15 @@
+// @ts-check
+
 import eslint from '@eslint/js';
+// @ts-ignore
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    ignores: [
+      "dist"
+    ]
+  }
 );
